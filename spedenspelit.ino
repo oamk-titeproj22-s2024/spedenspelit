8109f8c572bd69c0b84cd0c21f205568c961538e
Luukas Ahola <noname@example.com>
2024-12-02 13:35:43 +0200

    Toteutettu pelilogiikkatesti binäärijonoilla

--- spedenspelit.ino
+++ spedenspelit.ino
@@ -1,14 +1,15 @@
 #include <Arduino_FreeRTOS.h>
 #include <leds.h>
 
-
-
 int numero;
 bool result;
+const int bin = 0b1111100;
 
 void setup() {
   initializeLeds();
 // numero = random(2, 5);
+  DDRK = 0x183;
+  PORTK = 0x7C;
 
 xTaskCreate(MainTask, "Game", 100, NULL, 1, NULL);
 xTaskCreate(IdleTask, "WaitInit", 100, NULL, 0, NULL);
@@ -18,8 +19,17 @@ void loop() {
 }
 
 static void MainTask(void* pvParameters) {
-  show2(5);
-  vTaskSuspend(NULL);
+  numero = random(2, 5);
+  setLed(numero);
+  
+
+  if(PINK != bin & ~ (1 << (numero+1))) {
+    vTaskSuspend(NULL);
+  }
+  else {
+    setAllLeds();
+  }
+  // vTaskSuspend(NULL);
 }
 
 static void IdleTask(void* pvParameters) {
