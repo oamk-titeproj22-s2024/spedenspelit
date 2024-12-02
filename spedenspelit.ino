9dfc314f7edd28a0f864c37c4a6ec1505e4baeb3
Luukas Ahola <noname@example.com>
2024-12-02 22:12:33 +0200

    Korjattu nappien tilatarkistus

--- spedenspelit.ino
+++ spedenspelit.ino
@@ -1,39 +1,56 @@
 #include <Arduino_FreeRTOS.h>
 #include <leds.h>
 
+TaskHandle_t TaskHandle_1;
+TaskHandle_t TaskHandle_2;
+
 int numero;
+int binumero;
+int randomness[5];
+int randcount;
 bool result;
 const int bin = 0b1111100;
 
 void setup() {
   initializeLeds();
-// numero = random(2, 5);
   DDRK = 0x183;
   PORTK = 0x7C;
-
-xTaskCreate(MainTask, "Game", 100, NULL, 1, NULL);
-xTaskCreate(IdleTask, "WaitInit", 100, NULL, 0, NULL);
+//  numero = random(2, 5);
+//  binumero = bin & ~ (1 << (numero));
+xTaskCreate(MainTask, "Game", 100, NULL, 1, &TaskHandle_1);
+xTaskCreate(IdleTask, "WaitInit", 100, NULL, 0, &TaskHandle_2);
 }
 
 void loop() {
 }
 
 static void MainTask(void* pvParameters) {
-  numero = random(2, 5);
-  setLed(numero);
-  
 
-  if(PINK != bin & ~ (1 << (numero+1))) {
+    result = false;
+    numero = random(2, 5);
+    binumero = bin & ~ (1 << (numero));
+    setLed(numero-2);
+ 
+  if(PINK == binumero) {
+    result = true;
     vTaskSuspend(NULL);
   }
-  else {
-    setAllLeds();
+
+  if(PINK != binumero && PINK != bin) {
+    vTaskSuspend(NULL);
   }
+ 
   // vTaskSuspend(NULL);
 }
 
 static void IdleTask(void* pvParameters) {
-while(1) {
-  show1();
+//while(1) {
+//  show1();
+//  }
+
+while(result == true) {
+ numero = random(2, 5);
+ vTaskResume(TaskHandle_1);
   }
+  setAllLeds();
 }
