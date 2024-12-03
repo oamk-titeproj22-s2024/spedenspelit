0f9d0a0dbe9e8ea05d2688e72cf78b8bc7dec80e
Luukas Ahola <noname@example.com>
2024-12-03 11:36:07 +0200

    display korjattu

--- display.cpp
+++ display.cpp
@@ -1,3 +1,38 @@
+#include <arduino.h>
+#include "display.h"
+const int ResetPin = 12;
+const int shiftClockPin = 11;
+const int latchClockPin = 10;
+const int outEnablePin = 9;
+const int serialInputPin = 8;
+
+const uint8_t numerot[10] = {
+    0b01111110, // 0
+    0b00110000, // 1
+    0b01101101, // 2
+    0b01111001, // 3
+    0b00110011, // 4
+    0b01011011, // 5
+    0b01011111, // 6
+    0b01110000, // 7
+    0b01111111, // 8
+    0b01111011  // 9
+
+};
+
+void initializeDisplay(void)
+{
+    pinMode(ResetPin, OUTPUT);
+    pinMode(shiftClockPin, OUTPUT);
+    pinMode(latchClockPin, OUTPUT);
+    pinMode(outEnablePin, OUTPUT);
+    pinMode(serialInputPin, OUTPUT);
+
+    digitalWrite(ResetPin, HIGH);
+    digitalWrite(outEnablePin, LOW);
+    digitalWrite(latchClockPin, LOW);
+}
+
 void writeByte(uint8_t bits, bool last)
 {
     digitalWrite(serialInputPin, 1);
