a034b42387b76ab0f81b8befe1ee80cf778ba6ab
Luukas Ahola <noname@example.com>
2024-12-03 11:34:29 +0200

    lisaa

--- spedenspelit.ino
+++ spedenspelit.ino
@@ -3,9 +3,10 @@
 
 TaskHandle_t TaskHandle_1;
 TaskHandle_t TaskHandle_2;
+TaskHandle_t TaskHandle_3;
 
-int numero;
-int binumero;
+volatile int numero;
+volatile int binumero;
 int randomness[5];
 int randcount;
 bool result;
@@ -17,16 +18,24 @@ void setup() {
   PORTK = 0x7C;
 //  numero = random(2, 5);
 //  binumero = bin & ~ (1 << (numero));
+//xTaskCreate(StartTask, "Start", 100, NULL, 2, &TaskHandle_3);
 xTaskCreate(MainTask, "Game", 100, NULL, 1, &TaskHandle_1);
 xTaskCreate(IdleTask, "WaitInit", 100, NULL, 0, &TaskHandle_2);
 }
 
 void loop() {
 }
+//
+//static void StartTask(void* pvParameters) {
+//  numero = random(2, 5);
+//  binumero = bin & ~ (1 << (numero));
+//  vTaskSuspend(NULL);
+////vTaskResume(TaskHandle_1);
+//}
 
 static void MainTask(void* pvParameters) {
-
-    result = false;
+//
+//result = false;
     numero = random(2, 5);
     binumero = bin & ~ (1 << (numero));
     setLed(numero-2);
@@ -47,7 +56,6 @@ static void IdleTask(void* pvParameters) {
 //while(1) {
 //  show1();
 //  }
-
 while(result == true) {
  numero = random(2, 5);
  vTaskResume(TaskHandle_1);
