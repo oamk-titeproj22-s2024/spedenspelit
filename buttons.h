07f18737f3072fb0daa35af1d2520b8f358fd18b
Luukas Ahola <luukas.ahola@gmail.com>
2024-12-05 08:57:58 +0200

    Update buttons.h

--- buttons.h
+++ buttons.h
@@ -3,23 +3,16 @@
 #include <arduino.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+// Kommentti lisätty commitia varten
 
+const byte buttonPins[] = {2,3,4,5,6};
+const byte numButtons = sizeof(buttonPins) / sizeof(buttonPins[0]);
 
-const byte firstPin = 2; // First PinChangeInterrupt on D-bus
-const byte lastPin =  5; // Last PinChangeInterrupt on D-bus
+extern volatile unsigned long lastDebounceTime[numButtons];
+const unsigned long debounceDelay = 150;
 
-/* 
-  initButtonsAndButtonInterrupts subroutine is called from Setup() function
-  during the initialization of Speden Spelit. This function does the following:
-  1) Initializes 4 button pins for the game = Arduino pins 2,3,4,5
-  2) Initializes 1 button pin for starting the game = Aruino pin 6
-  3) Enables PinChangeInterrupt on D-bus in a way that interrupt
-     is generated whenever some of pins 2,3,4,5,6 is connected to LOW state
-
-*/
 void initButtonsAndButtonInterrupts(void);
 
 
-// Intoduce PCINT2_vect Interrupt SeRvice (ISR) function for Pin Change Interrupt.
-ISR(PCINT2_vect); 
-#endif;
+ISR(PCINT2_vect);
+#endif
