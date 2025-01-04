40bb64a7886bc28abd52214596565d74dd8c9a84
Luukas Ahola <luukas.ahola@gmail.com>
2025-01-04 12:56:01 +0200

    Lisää kommentit logiikkakirjastoon

--- logic.cpp
+++ logic.cpp
@@ -1,43 +1,43 @@
-#include "logic.h"
-#include "display.h"
+#include "logic.h"    
+#include "display.h"    
 #include "buttons.h"
 
-volatile byte numero;
-volatile uint8_t userNumbers[99];
-volatile uint8_t randomNumbers[99];
-volatile byte userIndex = 0;
-volatile byte randomIndex = 0;
-extern volatile bool timeToCheckGameStatus;
-extern volatile byte buttonNumber;
-volatile bool timeToMakeNewNumber;
-extern volatile bool started;
+volatile byte numero;    // viimeisin satunnaisnumero
+volatile uint8_t userNumbers[99];    // painallustaulukko
+volatile uint8_t randomNumbers[99];  // satunnaisnumeroiden taulukko
+volatile byte userIndex = 0;     // painallustaulukon järjestysnumero 
+volatile byte randomIndex = 0;   // satunnaisnumeroiden taulukon järjestysnumero
+extern volatile bool timeToCheckGameStatus;    // boolean pelitilanteen tarkistamiselle
+extern volatile byte buttonNumber;    // painetun napin järjestysnumero nappikirjaston taulukossa
+volatile bool timeToMakeNewNumber;    // boolean uuden satunnaisnumeron luomiselle
+extern volatile bool started;    // boolean pelin käynnistykselle
 
 
 void initializeTimer() {
-  cli();
+  cli();    // pysäytä kaikki aktiiviset keskeytyspalvelijat
   TCCR1A = 0;           // Init Timer1A
   TCCR1B = 0;           // Init Timer1B
   TCCR1B |= (1 << WGM12);
   TCCR1B |= (1 << CS12); 
-  OCR1A = 62500;        // Timer Compare1A Register
+  OCR1A = 62500;        // 1Hz pelinopeus alussa
   TIMSK1 |= (1 << OCIE1A);  // Enable Timer COMPA Interrupt
-  sei();
+  sei();  // salli keskeytyspalvelijoiden toiminta
 }
 
   void initializeGame() {
-    userIndex = 0;
-    randomIndex = 0;
+    userIndex = 0;    // nollaa painallustaulukon järjestysnumero
+    randomIndex = 0;  // nollaa satunnaisnumeroiden taulukon järjestysnumero
   }
   
 void checkGame() 
 {
-  clearAllLeds();
+  clearAllLeds();    // sammuta kaikki ledit
       // Serial.println(userNumbers[userIndex-1]);
       // Serial.println(randomNumbers[randomIndex-1]);
       for(int i=0; i<userIndex; i++){ 
     if(userNumbers[i]!=randomNumbers[i])  
     {
-    stopTheGame();
+    stopTheGame();   // pysäytä peli painallustaulukon erotessa satunnaisnumeroiden taulukosta
 //      Serial.print("Peli päättyi, ");
 //      Serial.print("Lopullinen kierrosluku: ");
 //      Serial.println(userIndex);
@@ -46,14 +46,14 @@ void checkGame()
     }
   if(userIndex == 99)
   {
-    stopTheGame();
+    stopTheGame(); // pysäytä peli kierrosluvun ollessa 99 näyttöjen salliessa vain kaksinumeroisen kierrosluvun
     Serial.println("Voitit pelin");
     return;
   }
 //  Serial.print("Oikea painallus, ");
 //  Serial.print("Kierrosluku: ");
 //  Serial.println(userIndex);
-  showResult(userIndex);
+  showResult(userIndex);  // Näytä kierrosluku näytöillä
 
   if(randomIndex % 10 == 0)
   {
@@ -64,20 +64,20 @@ void checkGame()
 }
 
 void startTheGame() {
-  clearAllLeds();
-  initializeTimer();
-  initializeGame();
-  initializeDisplay();
-  initButtonsAndButtonInterrupts();
-  delay(500);
+  clearAllLeds();    // Sammuta ledit
+  initializeTimer();   // aktivoi ajastinpohjainen keskeytysrutiini
+  initializeGame();    // nollaa taulukoiden järjestysnumerot
+  initializeDisplay(); // alusta näytöt
+  initButtonsAndButtonInterrupts();    // alusta napit
+  delay(500);    // odota 0.5 sekuntia
 }
 
 void stopTheGame(){
-  started = false;
-  TIMSK1 &=~(1<<OCIE1A);
-  showResult(userIndex);
-  clearAllLeds();
-  setAllLeds();
+  started = false;    // aseta pelin käynnistystilanne epätodeksi
+  TIMSK1 &=~(1<<OCIE1A);    // pysäytä ajastin
+  showResult(userIndex);    // näytä kierrosluku näytöillä
+  clearAllLeds();           // sammuta ledit
+  setAllLeds();             // sytytä kaikki ledit
 }
 
 ISR(TIMER1_COMPA_vect) 
